8eb0eb6cde574a818c5f3c1a297c4bab05c54fb3
Markus Klepping <markus.klepping@code.berlin>
2024-05-17 23:16:12 +0200

    add loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-"use client";
+"use client"
 import React, { useEffect, useState } from "react";
 import { DateTime } from "luxon";
 import GenericCounter from "./generic";
 
 export default function Home() {
   const [timeLeft, setTimeLeft] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timeZone = 'Europe/Berlin';
     const endDate = DateTime.fromISO('2024-08-01T00:00:00', { zone: timeZone }).toUTC();
 
-    
     const calculateTimeLeft = () => {
       const now = DateTime.now().setZone(timeZone);
       const timeDiff = endDate.diff(now, ['days', 'hours', 'minutes', 'seconds']).toObject();
@@ -24,6 +24,7 @@ export default function Home() {
 
     const updateTimeLeft = () => {
       setTimeLeft(calculateTimeLeft());
+      setIsLoading(false); 
     };
 
     const interval = setInterval(updateTimeLeft, 1000);
@@ -36,15 +37,17 @@ export default function Home() {
       <div className="space-y-6">
         <h1 className="text-3xl font-bold text-center text-gray-800">Bis zu unserem Trip nach Italien...</h1>
         <div className="text-lg text-center bg-white p-4 rounded-lg shadow-md text-black">
-          {timeLeft.days} Tage, {timeLeft.hours} Stunden, {timeLeft.minutes} Minuten, {timeLeft.seconds} Sekunden
+          {
+            isLoading ? "Wird berechnet..." : 
+            `${timeLeft.days} Tage, ${timeLeft.hours} Stunden, ${timeLeft.minutes} Minuten, ${timeLeft.seconds} Sekunden`
+          }
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-4xl mx-auto text-black ">
-        <GenericCounter label="... werden noch ca." totalAmount={26500000000} unit="€ an Bürgergeld ausgezahlt" />
-        <GenericCounter label="... wird uns der Bundestag noch." totalAmount={1200000000} unit="€ kosten" />
-        <GenericCounter label="... wird die DB noch" totalAmount={3700800} unit="Minuten zu spät kommen" />
+          <GenericCounter label="... werden noch ca." totalAmount={26500000000} unit="€ an Bürgergeld ausgezahlt" />
+          <GenericCounter label="... wird uns der Bundestag noch." totalAmount={1200000000} unit="€ kosten" />
+          <GenericCounter label="... wird die DB noch" totalAmount={3700800} unit="Minuten zu spät kommen" />
           <GenericCounter label="... werden noch ca." totalAmount={50000000000} unit="Burger in den USA gegessen" />
           <GenericCounter label="... wird Lidl noch ca." totalAmount={114800000000} unit="€ an Umsatz generieren" />
-          
         </div>
       </div>
     </main>
